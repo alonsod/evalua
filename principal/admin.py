369f99d6ddbf351ef1f04e311a0264454c3d0f0b
Daniel <d_alonsoc@yahoo.com.mx>
2012-06-27 19:14:02 -0500

    modelo

---
+++ principal/admin.py
@@ -0,0 +1,6 @@
+from principal.models import Curso, Cuestionario
+from django.contrib import admin
+
+
+admin.site.register(Curso)
+admin.site.register(Cuestionario)
--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,43 @@
+#encoding:utf-8
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Curso(models.Model):
+	curso_id = models.AutoField(primary_key=True)
+	nombre_curso = models.TextField()
+
+	def __unicode__(self):
+		return self.nombre_curso
+
+class Cuestionario(models.Model):
+	cuestionario_id = models.AutoField(primary_key=True)
+	nombre_cuestionario = models.TextField()
+
+	def __unicode__(self):
+		return self.nombre_cuestionario
+
+class Grupo(models.Model):		
+	grupo_id = models.AutoField(primary_key=True)
+	nombre_grupo = models.TextField()
+	fecha = models.DateTimeField()
+	curso = models.ForeignKey(Curso)
+	cuestionario = models.ForeignKey(Cuestionario)
+	usuario = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return self.nombre_grupo
+
+class Participante(models.Model):
+	participante_id = models.AutoField(primary_key=True)
+	nombre = models.TextField()
+
+	def __unicode__(self):
+		return self.nombre
+
+class DetalleGrupo(models.Model):
+	grupo = models.ForeignKey(Grupo)
+	participante = models.ForeignKey(Participante)
+	nombre = models.TextField()
+	usuario = models.ForeignKey(User)
+	fregistro = models.DateTimeField(auto_now=True)
+	
